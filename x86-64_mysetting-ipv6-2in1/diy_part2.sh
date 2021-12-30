6f2c74976175d28ef2690a5ad78c8572e9e10191
bonnyandsky <30990861+bonnyandsky@users.noreply.github.com>
2021-12-30 14:20:56 +0800

    去除拉取wrtbwmon，因为图形界面无法显示

--- x86-64_mysetting-ipv6-2in1/diy_part2.sh
+++ x86-64_mysetting-ipv6-2in1/diy_part2.sh
@@ -15,7 +15,7 @@ sed -i 's/192.168.1.1/192.168.0.1/g' package/base-files/files/bin/config_generat
 
 # 移除重复软件包
 # rm -rf package/lean/luci-app-netdata 原来的是英文
-rm -rf package/lean/luci-app-wrtbwmon
+# rm -rf package/lean/luci-app-wrtbwmon
 # rm -rf package/lean/luci-theme-argon 所以暂时不知道自带的主题wrtbwmon是否显示正常
 
 # 添加额外软件包，不在根目录要用svn co，然后tree/main替换成trunk
@@ -24,16 +24,16 @@ git clone https://github.com/esirplayground/luci-app-poweroff package/luci-app-p
 svn co https://github.com/sirpdboy/sirpdboy-package/trunk/luci-app-advanced package/luci-app-advanced
 svn co https://github.com/sirpdboy/sirpdboy-package/trunk/luci-app-netspeedtest package/luci-app-netspeedtest
 
-# 流量监控
-svn co https://github.com/sirpdboy/sirpdboy-package/trunk/luci-app-wrtbwmon package/luci-app-wrtbwmon
-svn co https://github.com/sirpdboy/sirpdboy-package/trunk/wrtbwmon package/wrtbwmon
+# 流量监控（21.02图形界面也无法显示）
+# svn co https://github.com/sirpdboy/sirpdboy-package/trunk/luci-app-wrtbwmon package/luci-app-wrtbwmon
+# svn co https://github.com/sirpdboy/sirpdboy-package/trunk/wrtbwmon package/wrtbwmon
 
 # Themes
 # git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
 # git clone https://github.com/jerrykuku/luci-app-argon-config package/luci-app-argon-config
 
 # 修改插件名字
-sed -i 's/"流量"/"实时流量监测"/g' `grep "流量" -rl ./`
+# sed -i 's/"流量"/"实时流量监测"/g' `grep "流量" -rl ./`
 
 ./scripts/feeds update -a
 ./scripts/feeds install -a
